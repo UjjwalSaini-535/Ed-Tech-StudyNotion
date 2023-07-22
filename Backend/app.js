c1546c6083d3674ad6561b3c9c33c0d1a74302e8
Ujjwal Saini <66915278+UjjwalSaini-535@users.noreply.github.com>
2023-07-23 05:07:38 +0530

    CORS Origin Change

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ed-tech-study-notion-front-end.vercel.app/",
     credentials: true,
   })
 );
