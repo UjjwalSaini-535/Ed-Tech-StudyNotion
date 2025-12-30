d16c701eeb46a51dba00757c06920f636a04e518
Ujjwal Saini <ujjwals.2011@gmail.com>
2025-12-31 02:39:02 +0530

    .

--- Backend/utils/mailSender.js
+++ Backend/utils/mailSender.js
@@ -2,13 +2,23 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
   try {
+    console.log("Sending mail to: ", email);
+
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: Number(process.env.MAIL_PORT),
+      secure: false, 
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
+      connectionTimeout: 10000,
     });
+
+    console.log("Transporter created: ", transporter);
 
     let info = await transporter.sendMail({
       from: "StudyNotion -by Ujjwal Saini",
